c4d0f42d62888b85f833e382f10ec615a73368e6
Alex <shadyalexcodes@gmail.com>
2019-01-06 15:19:57 -0800

    Removed Email Option

--- javascript/typed.js
+++ javascript/typed.js
@@ -5,7 +5,7 @@ jQuery(document).ready(function() {
             'I am a junior software engineer...',
             'I am a musician...',
             'I am always learning...',
-            'Find me at <br><a href="https://github.com/ShadyAlexCodes">Github</a> | <a href="https://twitter.com/ShadyAlexCodes">Twitter</a> | <a href="mailto:shadyalexcodes@gmail.com">Email</a> | <a href="https://discord.gg/prEeXGn">Discord</a> ',
+            'Find me at <br><a href="https://github.com/ShadyAlexCodes">Github</a> | <a href="https://twitter.com/ShadyAlexCodes">Twitter</a> | <a href="https://discord.gg/prEeXGn">Discord</a> ',
         ],
         typeSpeed: 40,
         backSpeed: 50,
