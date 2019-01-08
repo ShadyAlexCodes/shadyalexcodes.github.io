b77e9ab013b6da056bcf7ad0808f69f3d2131f30
Alex <shadyalexcodes@gmail.com>
2019-01-07 19:59:25 -0800

    Final Product

--- README.md
+++ README.md
@@ -6,10 +6,10 @@ A page to showcase all the work I've made.
 ### Extra
 [License](http://github.com)
 
-[Live Version](https://shadyalexcodes.github.io/Holiday-Calendar/index.html)
+[Live Version](shadyalexcodes.com)
 
 # Notice
 
-This is a photo of the page taken on 1/6/2019, but may not be acurate 
+This is a photo of the page taken on 1/7/2019, but may not be acurate 
 
-![alt text](https://i.imgur.com/OgRIrbl.png)
+![alt text](https://imgur.com/QMnXR17.png)
--- javascript/typed.js
+++ javascript/typed.js
@@ -2,10 +2,10 @@ jQuery(document).ready(function() {
     var message = new Typed('#message', {
         strings: [
             'I am a developer...',
-            'I am a junior software engineer...',
+            'I am a software engineer...',
             'I am a musician...',
             'I am always learning...',
-            'Find me at <br><a href="https://github.com/ShadyAlexCodes">Github</a> | <a href="https://twitter.com/ShadyAlexCodes">Twitter</a> | <a href="https://discord.gg/prEeXGn">Discord</a> | <a href="#">Projects</a>',
+            'Find me on <br><a href="https://github.com/ShadyAlexCodes">Github</a> | <a href="https://twitter.com/ShadyAlexCodes">Twitter</a> | <a href="https://discord.gg/prEeXGn">Discord</a>',
         ],
         typeSpeed: 40,
         backSpeed: 50,
