e1a1a5cfcb320d6f9262667325c25cbe59b83d43
Alex <shadyalexcodes@gmail.com>
2019-01-07 19:59:30 -0800

    Revert "Final Product"
    This reverts commit b77e9ab013b6da056bcf7ad0808f69f3d2131f30.

--- javascript/typed.js
+++ javascript/typed.js
@@ -2,10 +2,10 @@ jQuery(document).ready(function() {
     var message = new Typed('#message', {
         strings: [
             'I am a developer...',
-            'I am a software engineer...',
+            'I am a junior software engineer...',
             'I am a musician...',
             'I am always learning...',
-            'Find me on <br><a href="https://github.com/ShadyAlexCodes">Github</a> | <a href="https://twitter.com/ShadyAlexCodes">Twitter</a> | <a href="https://discord.gg/prEeXGn">Discord</a>',
+            'Find me at <br><a href="https://github.com/ShadyAlexCodes">Github</a> | <a href="https://twitter.com/ShadyAlexCodes">Twitter</a> | <a href="https://discord.gg/prEeXGn">Discord</a> | <a href="#">Projects</a>',
         ],
         typeSpeed: 40,
         backSpeed: 50,
